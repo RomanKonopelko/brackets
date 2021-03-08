eb4b12f2b6c396ca7bc816525c2b99fc7a7c46f9
RomanKonopelko <romank761@gmail.com>
2021-03-08 20:09:42 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    // your solution
+    let stack = [];
+    const getLastBracket = (stack) => stack[stack.length - 1];
+
+    for (let i = 0; i < str.length; i++) {
+        let letter = str.charAt(i);
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (letter === bracketsConfig[j][0]) {
+                if (
+                    (letter === "|" && getLastBracket(stack) === "|") ||
+                    (letter === "7" && getLastBracket(stack) === "7") ||
+                    (letter === "8" && getLastBracket(stack) === "8")
+                ) {
+                    stack.pop();
+                } else {
+                    stack.push(letter);
+                }
+            } else if (letter === bracketsConfig[j][1]) {
+                if (
+                    getLastBracket(stack) === bracketsConfig[j][0] ||
+                    getLastBracket(stack) === "|"
+                ) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return stack.length === 0;
+};
